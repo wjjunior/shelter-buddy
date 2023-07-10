0ca73cec69e4abf03bf2fc8929a0c4f07f21c33a
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2023-07-10 14:46:39 -0300

    feat: add header mobile style breakpoint

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -4,6 +4,9 @@ export const StyledHeader = styled('header')(({ theme }) => ({
   display: 'flex',
   justifyContent: 'center',
   margin: `${theme.spacing(12)} 0`,
+  [theme.breakpoints.down(900)]: {
+    margin: `58px 0`,
+  },
 }))
 
 export const StyledLogo = styled('img')(({ theme }) => ({
