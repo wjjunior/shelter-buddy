1557c835f9e1f83b021e732ee9c1f5bcfcd39b75
Wagner Junior <wagner.junior@pensiondynamics.com.br>
2023-07-10 14:51:37 -0300

    test: add a responsive pagination component

--- src/components/sortable-list/components/pagination/pagination.spec.tsx
+++ src/components/sortable-list/components/pagination/pagination.spec.tsx
@@ -1,6 +1,8 @@
 import { render, fireEvent } from '@testing-library/react'
 import Chance from 'chance'
 
+import { SORTABLE_LIST_ITEMS_PER_PAGE } from '../../../../utils/constants'
+
 import PaginationComponent from './pagination'
 import { PaginationComponentProps } from './types'
 
@@ -9,14 +11,16 @@ const chance = new Chance()
 const onPageChange = jest.fn()
 
 const makeSut = ({
-  totalPages = chance.integer({ min: 1, max: 10 }),
-  currentPage = chance.integer({ min: 1, max: totalPages }),
+  count = chance.integer({ min: 11, max: 100 }),
+  currentPage = 1,
+  isMobile = false,
 }: Partial<PaginationComponentProps> = {}) =>
   render(
     <PaginationComponent
-      totalPages={totalPages}
+      count={count}
       currentPage={currentPage}
       onPageChange={onPageChange}
+      isMobile={isMobile}
     />,
   )
 
@@ -26,40 +30,31 @@ describe('PaginationComponent', () => {
   })
 
   test('renders pagination buttons with correct active and inactive states', () => {
-    const totalPages = chance.integer({ min: 1, max: 10 })
-    const currentPage = chance.integer({ min: 1, max: totalPages })
-    const { getAllByRole } = makeSut({ totalPages, currentPage })
+    const count = chance.integer({ min: 11, max: 100 })
+    const currentPage = 1
+    const totalPages = Math.ceil(count / SORTABLE_LIST_ITEMS_PER_PAGE)
 
-    const buttons = getAllByRole('button')
+    const { getAllByTestId } = makeSut({ count, currentPage })
 
-    expect(buttons).toHaveLength(totalPages)
-
-    buttons.forEach((button, index) => {
-      const pageNumber = index + 1
-      const isActive = pageNumber === currentPage
-
-      expect(button).toHaveTextContent(pageNumber.toString())
-
-      if (isActive) {
-        expect(button).toHaveClass('makeStyles-activeButton-2')
-        expect(button).toHaveClass('MuiButton-contained')
-      } else {
-        expect(button).toHaveClass('makeStyles-inactiveButton-3')
-        expect(button).toHaveClass('MuiButton-outlined')
-      }
-    })
+    const paginationButtons = getAllByTestId('pagination-button')
+    expect(paginationButtons).toHaveLength(totalPages)
+    expect(paginationButtons[0]).toHaveTextContent('1')
+    expect(paginationButtons[0]).toHaveClass('makeStyles-activeButton-2')
   })
 
   test('calls onPageChange callback when a button is clicked', () => {
-    const totalPages = chance.integer({ min: 1, max: 10 })
+    const { getByText } = makeSut()
 
-    const { getByText } = makeSut({ totalPages })
+    const paginationButton = getByText('1')
+    fireEvent.click(paginationButton)
 
-    const buttonIndex = chance.integer({ min: 0, max: totalPages - 1 })
-    const button = getByText((buttonIndex + 1).toString())
-    fireEvent.click(button)
+    expect(onPageChange).toHaveBeenCalledWith(1)
+  })
 
-    expect(onPageChange).toHaveBeenCalledTimes(1)
-    expect(onPageChange).toHaveBeenCalledWith(buttonIndex + 1)
+  test('should render the TablePagination component when isMobile is true', () => {
+    const { getByTestId } = makeSut({ isMobile: true })
+
+    const tablePagination = getByTestId('table-pagination')
+    expect(tablePagination).toBeInTheDocument()
   })
 })
